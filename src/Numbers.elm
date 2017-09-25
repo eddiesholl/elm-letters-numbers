55c1f4b7858e3e1aaec7c040a52d1c8c82fb8ed6
Eddie Sholl <esholl@zendesk.com>
2017-09-25 22:41:47 +1000

    initial commit

---
+++ src/Numbers.elm
@@ -0,0 +1,109 @@
+-- Read more about this program in the official Elm guide:
+-- https://guide.elm-lang.org/architecture/effects/http.html
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+type alias Input = String
+type alias Inputs = List Input
+
+type alias Model =
+  { inputs : Inputs
+  }
+
+
+init : (Model, Cmd Msg)
+init =
+  ( Model []
+  , Cmd.none
+  )
+
+
+
+-- UPDATE
+
+
+type Msg
+  = UpdateInput Int Input
+  | AddInput
+  | InvalidInput
+
+alterInput : Inputs -> Int -> Input -> Inputs
+alterInput inputs ix newVal =
+  case (List.head inputs) of
+    Nothing ->
+      inputs
+    Just h ->
+      case (List.tail inputs) of
+        Nothing ->
+          [h]
+        Just t ->
+          case ix of
+            0 ->
+              newVal :: t
+            _ ->
+              h :: (alterInput t (ix - 1) newVal)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    UpdateInput ix newVal ->
+      ({ model | inputs = (alterInput model.inputs ix newVal) }, Cmd.none)
+    AddInput ->
+      ({ model | inputs = model.inputs ++ [""]}, Cmd.none)
+    InvalidInput ->
+      (model, Cmd.none)
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h2 [] [text "Hi"]
+    , div [] [(inputsView model)]
+    ]
+
+inputTyped : Int -> String -> Msg
+inputTyped ix input =
+  case String.toInt input of
+    Ok n ->
+      UpdateInput ix input
+    Err _ ->
+      InvalidInput
+
+inputView : Int -> Input -> Html Msg
+inputView ix inp =
+  div [] [
+    input [onInput (inputTyped ix), value inp] []
+    ]
+
+inputsView : Model -> Html Msg
+inputsView model =
+  div []
+    [ div [] (List.indexedMap inputView model.inputs)
+    , button [onClick AddInput] [text "add"]
+    ]
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
